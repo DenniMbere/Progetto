a221209b824a1464b51781175d0b90d2bfa0ddfa
Denni <denniscordio@hotmail.it>
2017-11-13 16:19:01 +0100

    Create Commessa class

---
+++ src/entities/Commessa.java
@@ -0,0 +1,44 @@
+package entities;
+
+public class Commessa 
+{
+	private String codice;
+	private String nome;
+	private int matDip;
+	private int idCliente;
+	public Commessa(String codice, String nome, int matDip, int idCliente) 
+    {	
+		this.codice = codice;
+		this.nome = nome;
+		this.matDip = matDip;
+		this.idCliente = idCliente;
+	}
+	public String getCodice() {
+		return codice;
+	}
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getMatDip() {
+		return matDip;
+	}
+	public void setMatDip(int matDip) {
+		this.matDip = matDip;
+	}
+	public int getIdCliente() {
+		return idCliente;
+	}
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+	
+	
+	
+
+}
