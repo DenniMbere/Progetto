c73acc6a9fc10a38bba0767a9528acd97bebd27e
Sync Lab <Sync Lab@DESKTOP-3P361HU>
2017-11-13 17:09:35 +0100

    Dipendente 1.0 by Matty

--- src/entities/Dipendente.java
+++ src/entities/Dipendente.java
@@ -1,5 +1,41 @@
 package entities;
 
-public class Dipendente {
+public class Dipendente 
+{
+	int matricola;
+	String nome;
+	String cognome;
+	
+	
+	
+	public Dipendente(int matricola, String nome, String cognome) {
+		super();
+		this.matricola = matricola;
+		this.nome = nome;
+		this.cognome = cognome;
+	}
+	
+	public Dipendente() {}
+	
+	public int getMatricola() {
+		return matricola;
+	}
+	public void setMatricola(int matricola) {
+		this.matricola = matricola;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	
 
 }
